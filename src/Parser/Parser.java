855db3e20ddc03deb823e0e53dc589f6f9e9b5d9
Johny Mehlabo <manestev@gmail.com>
2024-02-22 22:04:28 +0100

    Finished basic parser

---
+++ src/Parser/Parser.java
@@ -0,0 +1,40 @@
+package Parser;
+
+import Lexer.Token;
+import Lexer.TokenType;
+import Parser.Stmts.*;
+
+import java.util.List;
+
+public class Parser {
+
+    public static List<Token> tokens;
+
+    public static Token at() {
+        return tokens.get(0);
+    }
+
+    public static Token eat() {
+        return tokens.remove(0);
+    }
+
+    public static Token expect(TokenType tokenType, String err) {
+        if (at().kind != tokenType) {
+            System.err.println(err);
+            System.exit(-1);
+        }
+        return eat();
+    }
+
+    public static Stmt parseStmt() {
+        return switch (at().kind) {
+            case Var -> VarDeclarationStmt.parse();
+            default -> null;
+        };
+    }
+
+    public static Stmt parse(List<Token> tks){
+        tokens = tks;
+        return ProgramStmt.parse();
+    }
+}
